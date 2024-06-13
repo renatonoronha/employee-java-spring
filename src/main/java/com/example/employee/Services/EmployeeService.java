9a514aafb87a39bc547caff710db9eef52f80a03
Renato Noronha <dev.renato10@gmail.com>
2024-06-13 11:58:52 -0300

    feat: create Employee service

---
+++ src/main/java/com/example/employee/Services/EmployeeService.java
@@ -0,0 +1,65 @@
+package com.example.employee.Services;
+
+import com.example.employee.Models.Employee;
+import com.example.employee.Repositories.EmployeeRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+// Service layer is where all the business logic lies
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmployeeService {
+
+    @Autowired
+    private final EmployeeRepo employeeRepo;
+
+    public List<Employee> getAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee getEmployeeById(Integer id)
+    {
+        Optional<Employee> optionalEmployee = employeeRepo.findById(id);
+        if (optionalEmployee.isPresent()) {
+            return optionalEmployee.get();
+        }
+        log.info("Employee with id: {} doesn't exist", id);
+        return null;
+    }
+
+    public Employee saveEmployee(Employee employee)
+    {
+        employee.setCreatedAt(LocalDateTime.now());
+        employee.setUpdatedAt(LocalDateTime.now());
+        Employee savedEmployee = employeeRepo.save(employee);
+
+        log.info("Employee with id: {} saved successfully", employee.getId());
+        return savedEmployee;
+    }
+
+    public Employee updateEmployee(Employee employee)
+    {
+        Optional<Employee> existingEmployee = employeeRepo.findById(employee.getId());
+        employee.setCreatedAt(existingEmployee.get().getCreatedAt());
+        employee.setUpdatedAt(LocalDateTime.now());
+
+        Employee updatedEmployee = employeeRepo.save(employee);
+
+        log.info("Employee with id: {} updated successfully", employee.getId());
+        return updatedEmployee;
+    }
+
+    public void deleteEmployeeByID(Integer id)
+    {
+        employeeRepo.deleteById(id);
+    }
+
+}
